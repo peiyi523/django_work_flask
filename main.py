ef5eddad4904452d544d9d52a47f80f8eb0c5a06
peiyi <peiyi523@gmail.com>
2024-03-20 19:23:49 +0800

    使用echarts完成當月eps試算柱狀圖繪製

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 from flask import Flask, render_template
 from datetime import datetime
+from eps import get_eps
+import json
 
 app = Flask(__name__)
 
 
+@app.route("/current-EPS-charts")
+def current_EPS_charts():
+    return render_template("eps.html")
+
+
+@app.route("/current-EPS-data", methods=["GET"])  # 如果是很私密的資料要改成POST
+def current_EPS_data():
+    columns, values = get_eps()
+    company = [value[0] for value in values]
+    current_EPS = [value[6] for value in values]
+    result = json.dumps(
+        {"company": company, "current_EPS": current_EPS}, ensure_ascii=False
+    )
+    return result
+
+
 @app.route("/")
-def get_eps():
-    eps = {"元大證券": {"資本額": "659", "2月稅後損益 ": "12", "2月稅後EPS": "0.179"}}
-    return render_template("eps.html", eps=eps)
+def eps_table():
+    columns, values = get_eps()
+    # print(columns, values)
+    return render_template("eps.html", columns=columns, values=values)
 
 
+print(current_EPS_data())
 app.run(debug=True)
