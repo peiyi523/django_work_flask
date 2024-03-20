d81fe3ef3147db7ba988827c932a28c91a534861
peiyi <peiyi523@gmail.com>
2024-03-20 22:01:48 +0800

    把圖表用另開新頁呈現

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/current-EPS-charts")
 def current_EPS_charts():
-    return render_template("eps.html")
+    return render_template("eps-charts.html")
 
 
 @app.route("/current-EPS-data", methods=["GET"])  # 如果是很私密的資料要改成POST
@@ -16,6 +16,7 @@ def current_EPS_data():
     columns, values = get_eps()
     company = [value[0] for value in values]
     current_EPS = [value[6] for value in values]
+
     result = json.dumps(
         {"company": company, "current_EPS": current_EPS}, ensure_ascii=False
     )
